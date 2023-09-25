968ace8d501f9b945a653274aabe3688dc545056
sahaskamble <sahaskamble282@gmail.com>
2023-09-25 11:49:27 +0530

    recent/changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,10 @@ const bar = document.getElementById('navbar');
 //Open navbar menu
 opennav.addEventListener('click', ()=> {
   bar.classList.toggle('active');
+
+  if (closenav.style.cursor !== 'pointer') {
+    closenav.style.cursor = 'pointer';
+  }
 });
 
 //Close navbar menu
@@ -14,5 +18,6 @@ closenav.addEventListener('click', ()=> {
   //some fucking logic for my ego
   if (bar.classList.contains('active')) {
     bar.classList.remove('active');
+    closenav.style.cursor = 'default';
   }
 });
