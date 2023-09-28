42253fd5d41b131f4d8921769899ced472f3a734
sahaskamble <sahaskamble282@gmail.com>
2023-09-29 01:06:15 +0530

    Initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,53 +1,23 @@
-const opennav = document.getElementById('hamburger');
-const bar = document.getElementById('navbar');
+const opennav = document.getElementById("hamburger")
+const bar = document.getElementById("navbar")
 
 //Open navbar menu
-opennav.addEventListener('click', () => {
-  bar.classList.toggle('active');
+opennav.addEventListener("click", () => {
+  bar.classList.toggle("active")
 
-  if (closenav.style.cursor !== 'pointer') {
-    closenav.style.cursor = 'pointer';
+  if (closenav.style.cursor !== "pointer") {
+    closenav.style.cursor = "pointer"
   }
-});
+})
 
 //Close navbar menu
-const closenav = document.getElementById('closenav');
+const closenav = document.getElementById("closenav")
 
-closenav.addEventListener('click', () => {
-
+closenav.addEventListener("click", () => {
   //some fucking logic for my ego
-  if (bar.classList.contains('active')) {
-    bar.classList.remove('active');
-    closenav.style.cursor = 'default';
+  if (bar.classList.contains("active")) {
+    bar.classList.remove("active")
+    closenav.style.cursor = "default"
   }
-});
+})
 
-//Window Scroll for Elements
-
-var startScrollpos = window.scrollY;
-
-window.onscroll = () => {
-  var currentScrollpos = window.scrollY;
-
-  if (startScrollpos > currentScrollpos) {
-    document.getElementById('scrollbtn').style.display = 'block';
-  } else {
-    document.getElementById('scrollbtn').style.display = 'none';
-  }
-  startScrollpos = currentScrollpos;
-}
-
-var x = window.matchMedia("(max-width: 700px)");
-const arrow = document.getElementById('scrollbtn');
-
-function myfun(x) {
-  if (x.matches) {
-    arrow.classList.toggle('hide');
-  } else {
-    arrow.classList.remove('hide');
-  }
-}
-
-myfun(x)
-
-x.addListener(myfun())
