9cb6fd10245e546654a129e3dc69d7b1388cde0e
sahaskamble <sahaskamble282@gmail.com>
2023-10-10 14:19:58 +0530

    new theme

--- src/global/index.js
+++ src/global/index.js
@@ -1,23 +1,8 @@
-const opennav = document.getElementById("hamburger")
-const bar = document.getElementById("navbar")
+const ham = document.getElementById('hamburger');
+const opnav = document.getElementById('navbar');
 
-//Open navbar menu
-opennav.addEventListener("click", () => {
-  bar.classList.toggle("active")
-
-  if (closenav.style.cursor !== "pointer") {
-    closenav.style.cursor = "pointer"
-  }
-})
-
-//Close navbar menu
-const closenav = document.getElementById("closenav")
-
-closenav.addEventListener("click", () => {
-  //some fucking logic for my ego
-  if (bar.classList.contains("active")) {
-    bar.classList.remove("active")
-    closenav.style.cursor = "default"
-  }
-})
-
+ham.addEventListener("click", ()=> { 
+    opnav.classList.toggle('active');
+    opnav.classList.toggle('hidden');    
+    ham.classList.toggle('change');
+});
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss"
 
 export function GET(context) {
   return rss({
-    title: 'My Story',
-    description: 'So info about me and CV for recruters',
+    title: "My Portfolio",
+    description: "So info about me an my CV",
     site: context.site,
     items: [],
     customData: `<language>en-us</language>`,
-  });
+  })
 }
