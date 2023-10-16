7f3052151161ceb39c4ef4c6b1fda35fec6843d1
sahaskamble <sahaskamble282@gmail.com>
2023-10-17 00:04:56 +0530

    all closed

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://sahaskamble.github.io',
-  integrations: [tailwind()]
-});
--- src/global/index.js
+++
@@ -1,7 +0,0 @@
-const ham = document.getElementById("hamburger")
-const opnav = document.getElementById("menu-items")
-
-ham.addEventListener("click", () => {
-    opnav.classList.toggle("active")
-    ham.classList.toggle("change")
-})
