49704b14945949d8a96a62685e3a85496c13dac5
sahaskamble <sahaskamble282@gmail.com>
2023-10-26 13:07:29 +0530

    Re-design

--- src/global/index.js
+++ src/global/index.js
@@ -1,28 +1,21 @@
 // all variables are here...
-const ham = document.getElementById('hamburger');
-const burger = document.getElementById('burger');
-const dark_mode_switcher = document.getElementById('theme_switcher'); 
+const ham = document.getElementById("hamburger");
+const burger = document.getElementById("burger");
+const dark_mode_switcher = document.getElementById("theme_switcher");
 // const userTheme = localStorage.getItem('theme');
 // const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 //Navbar toggle...
-ham.addEventListener('click', () => {
-	burger.classList.toggle('active');
+ham.addEventListener("click", () => {
+  burger.classList.toggle("active");
 });
 
 // theme toggle
-dark_mode_switcher.addEventListener('click', () => {
-	if (document.documentElement.classList.contains('dark')) {
-		document.documentElement.classList.remove('dark');
-		return;
-	}else {
-		document.documentElement.classList.add('dark');
-	}
+dark_mode_switcher.addEventListener("click", () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    return;
+  } else {
+    document.documentElement.classList.add("dark");
+  }
 });
-
-
-
-
-
-
-
